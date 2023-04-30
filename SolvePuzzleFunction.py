618e3e7161f102a890ffeff1ce93874f869d9584
Rayyan Zaid <rayyanzaid0401@gmail.com>
2023-04-29 17:05:30 -0700

    added a stub for solve puzzle function

---
+++ SolvePuzzleFunction.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def solve_puzzle(intialBoard : List[str] , algorithmNumber : int):
+
+    numberOfNodesExpanded = 0
+    maxNumberOfNodesInQueue = 0
+    depthOfGoal = 0
+    finalStateNode = 0
+    
+    return (numberOfNodesExpanded, maxNumberOfNodesInQueue, depthOfGoal, finalStateNode)
