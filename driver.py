d02bafefa198a0fc5a7ce2f051ed4c038716ebbc
Rayyan Zaid <rayyanzaid0401@gmail.com>
2023-05-01 23:26:56 -0700

    changed board to 2D array

--- driver.py
+++ driver.py
@@ -2,7 +2,9 @@ from typing import List
 from SolvePuzzleFunction import solve_puzzle
 
 
-intialBoard : List[str] = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "*"]
+intialBoard : List[List[str]] = [["1" , "2" , "3"], 
+                                 ["4" , "5" , "6"], 
+                                 ["7" , "8" , "*"]]
 algorithmNumber = 1
 print(solve_puzzle(intialBoard, algorithmNumber))
 
