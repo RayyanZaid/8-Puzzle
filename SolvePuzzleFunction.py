6dda377f65b9dd5b6665d7a60a56aef34efca8c9
Rayyan Zaid <rayyanzaid0401@gmail.com>
2023-05-01 22:03:23 -0700

    started

--- SolvePuzzleFunction.py
+++ SolvePuzzleFunction.py
@@ -1,11 +1,24 @@
 from typing import List
 
 
-def solve_puzzle(intialBoard : List[str] , algorithmNumber : int):
+def solve_puzzle(initialBoard : List[str] , algorithmNumber : int):
+
+    # variables that need to be returned 
 
     numberOfNodesExpanded = 0
     maxNumberOfNodesInQueue = 0
     depthOfGoal = 0
     finalStateNode = 0
+    isSuccessful = False
     
-    return (numberOfNodesExpanded, maxNumberOfNodesInQueue, depthOfGoal, finalStateNode)
+    # create queue
+
+    node_queue = [initialBoard]
+
+    while node_queue:
+        if len(node_queue) == 0:
+            return (numberOfNodesExpanded, maxNumberOfNodesInQueue, depthOfGoal, finalStateNode, isSuccessful)
+
+
+    isSuccessful = True
+    return (numberOfNodesExpanded, maxNumberOfNodesInQueue, depthOfGoal, finalStateNode, isSuccessful)
