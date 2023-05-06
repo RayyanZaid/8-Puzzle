b1ee4b803c38ae92ab22ac1946da3867757a2437
Rayyan Zaid <rayyanzaid0401@gmail.com>
2023-05-06 14:06:05 -0700

    finished

--- driver.py
+++ driver.py
@@ -17,66 +17,74 @@ algorithmNumber = input()
 
 
 result = solve_puzzle(initialBoard, algorithmNumber)
+isSolved = result[0]
+if(isSolved):
+    print(f"To solve this problem, the search algorithm expanded a total of {result[0]} nodes.")
+    print(f"The maximum number of nodes in the queue at any one time: {result[1]}")
+    print(f"The depth of the goal node was {result[2]}")
+    print("EXTRA CREDIT:")
+    list_of_states = returnListOfStates(result[3])
+    for eachState in list_of_states:
+        print(eachState)
+        print()
 
-print("EXTRA CREDIT:")
-list_of_states = returnListOfStates(result[3])
-
-for eachState in list_of_states:
-    print(eachState)
-    print()
-
-
-print("EASY")
-result = solve_puzzle([["1","2","0"],
-                   ["4","5","3"],
-                   ["7","8","6"]],2)
-
-
-print(f"To solve this problem, the search algorithm expanded a total of {result[0]} nodes.")
-print(f"The maximum number of nodes in the queue at any one time: {result[1]}")
-print(f"The depth of the goal node was {result[2]}")
-
-print()
-print("EXTRA CREDIT:")
-list_of_states = returnListOfStates(result[3])
-
-for eachState in list_of_states:
-    print(eachState)
-    print()
-
-print("DOABLE")
-result = solve_puzzle([["0","1","2"],
-                   ["4","5","3"],
-                   ["7","8","6"]],2)
+   
 
 
-print(f"To solve this problem, the search algorithm expanded a total of {result[0]} nodes.")
-print(f"The maximum number of nodes in the queue at any one time: {result[1]}")
-print(f"The depth of the goal node was {result[2]}")
-
-print()
-print("EXTRA CREDIT:")
-list_of_states = returnListOfStates(result[3])
-
-for eachState in list_of_states:
-    print(eachState)
-    print()
 
 
-print("OH BOY")
-result = solve_puzzle([["8","7","1"],
-                   ["6","0","2"],
-                   ["5","4","3"]],2)
+
+# print("EASY")
+# result = solve_puzzle([["1","2","0"],
+#                    ["4","5","3"],
+#                    ["7","8","6"]],2)
 
 
-print(f"To solve this problem, the search algorithm expanded a total of {result[0]} nodes.")
-print(f"The maximum number of nodes in the queue at any one time: {result[1]}")
-print(f"The depth of the goal node was {result[2]}")
+# print(f"To solve this problem, the search algorithm expanded a total of {result[0]} nodes.")
+# print(f"The maximum number of nodes in the queue at any one time: {result[1]}")
+# print(f"The depth of the goal node was {result[2]}")
 
-print()
-print("EXTRA CREDIT:")
-list_of_states = returnListOfStates(result[3])
+# print()
+# print("EXTRA CREDIT:")
+# list_of_states = returnListOfStates(result[3])
 
-for eachState in list_of_states:
-    print(eachState)
-    print()
+# for eachState in list_of_states:
+#     print(eachState)
+#     print()
+
+# print("DOABLE")
+# result = solve_puzzle([["0","1","2"],
+#                    ["4","5","3"],
+#                    ["7","8","6"]],2)
+
+
+# print(f"To solve this problem, the search algorithm expanded a total of {result[0]} nodes.")
+# print(f"The maximum number of nodes in the queue at any one time: {result[1]}")
+# print(f"The depth of the goal node was {result[2]}")
+
+# print()
+# print("EXTRA CREDIT:")
+# list_of_states = returnListOfStates(result[3])
+
+# for eachState in list_of_states:
+#     print(eachState)
+#     print()
+
+
+# print("OH BOY")
+# result = solve_puzzle([["8","7","1"],
+#                    ["6","0","2"],
+#                    ["5","4","3"]],2)
+
+
+# print(f"To solve this problem, the search algorithm expanded a total of {result[0]} nodes.")
+# print(f"The maximum number of nodes in the queue at any one time: {result[1]}")
+# print(f"The depth of the goal node was {result[2]}")
+
+# print()
+# print("EXTRA CREDIT:")
+# list_of_states = returnListOfStates(result[3])
+
+# for eachState in list_of_states:
+#     print(eachState)
+#     print()
